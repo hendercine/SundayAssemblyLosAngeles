5fef40c6f54ffd8d2eb7bc3efc34f8d35d451f14
hendercine <james@hendercine.com>
2018-06-14 13:08:15 -0700

    feat: Add WebView layout and Widget java classes

---
+++ app/src/main/java/com/hendercine/sala/widget/SalaWidgetRemoteViewsService.java
@@ -0,0 +1,17 @@
+/*
+ * Created by James Henderson on 2018
+ * Copyright (c) Hendercine Productions and James Henderson 2018.
+ * All rights reserved.
+ *
+ * Last modified 6/14/18 1:06 PM
+ */
+
+package com.hendercine.sala.widget;
+
+import android.widget.RemoteViewsService;
+
+/**
+ * sala created by hendercine on 6/14/18.
+ */
+public class SalaWidgetRemoteViewsService extends RemoteViewsService {
+}
