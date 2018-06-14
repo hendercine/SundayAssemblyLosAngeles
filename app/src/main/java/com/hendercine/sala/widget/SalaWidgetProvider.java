e672dff4a91a997fdf509cc5b5c83ac6bb93de29
hendercine <james@hendercine.com>
2018-06-14 13:43:15 -0700

    feat: Begin build of widget layout and list item

--- app/src/main/java/com/hendercine/sala/widget/SalaWidgetProvider.java
+++ app/src/main/java/com/hendercine/sala/widget/SalaWidgetProvider.java
@@ -25,7 +25,7 @@ public class SalaWidgetProvider extends AppWidgetProvider {
 
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.salawidget);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.sala_widget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
