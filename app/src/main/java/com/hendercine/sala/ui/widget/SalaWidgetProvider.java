46f33e48c5ae305e0a74b797d73ae2fbe3a8111e
hendercine <james@hendercine.com>
2018-06-25 23:31:02 -0700

    feat: Build widget layout on release variant

--- app/src/main/java/com/hendercine/sala/ui/widget/SalaWidgetProvider.java
+++ app/src/main/java/com/hendercine/sala/ui/widget/SalaWidgetProvider.java
@@ -23,10 +23,10 @@ public class SalaWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        CharSequence widgetText = context.getString(R.string.appwidget_static_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.sala_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.sala_widget_static_text, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
